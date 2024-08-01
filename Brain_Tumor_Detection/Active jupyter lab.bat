bad877d3534b42248d5840bf64d52e894cd71d12
SyntaxSorcere <148754651+SyntaxSorcere@users.noreply.github.com>
2024-08-01 15:24:40 +0530

    Brain Tumor Detection Using CNN Algorithm
    Using CNN Algorithm

---
+++ Brain_Tumor_Detection/Active jupyter lab.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start "" "%windir%\System32\cmd.exe" /K "C:\Users\ARUN\Anaconda3\Scripts\activate.bat && cd /d "C:\Users\ARUN\OneDrive\Desktop\ALL FOLDER\PROJECT\Brain_Tumor_Detection" && conda activate python-cvcourse && jupyter lab"
